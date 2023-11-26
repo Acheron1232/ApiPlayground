b38c48be1a2d170b6c013f90e58ab66ebd2b9d00
Acheron1232 <aryemfedorov@gmail.com>
2023-11-26 14:38:26 +0200

    first commit

---
+++ src/main/java/ua/acheron/api_playground/ApiPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package ua.acheron.api_playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPlaygroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/acheron/api_playground/ApiPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.acheron.api_playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
