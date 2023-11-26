f3680097f7b262c3e478e83e60c6ae6d8bee4d82
bohdan <bohdenshoshka@gmail.com>
2023-11-26 23:03:08 +0200

    added geolocation interface

--- src/main/resources/static/js/foreign.js
+++ src/main/resources/static/js/foreign.js
@@ -1,12 +1,25 @@
-const country = document.querySelector(".getCountry");
-const infocountry = document.querySelector(".getCountry2");
-
 const userData = {
    position: {
       lotitude: undefined,
       longitude: undefined
-   }
+   },
+   geoGui: {
+      country :document.querySelector(".g-country p"),
+      city: document.querySelector(".g-city p"),
+      changeGeo(args){
+         const {country, city} = args;
+         country ?? (this.country.textContent = country);
+         city ?? (this.country.textContent = city);
+      },
+      getGeo(){
+         return {country: this.country.textContent,
+         city: this.city.textContent}
+      }
+   },
 };
+
+console.log(userData);
+console.log(userData.geoGui.getGeo());
 
 navigator.geolocation.getCurrentPosition(
    function (position) {
@@ -19,20 +32,21 @@ navigator.geolocation.getCurrentPosition(
       console.log("could not acess location");
    }
 );
-country.addEventListener("click", function () {
-   fetch("https://restcountries.com/v3.1/name/deutschland")
-      .then((response) => response.json())
-      .then((data) => {
-         console.log(data);
-      });
-});
 
-infocountry.addEventListener("click", function () {
-   fetch(
-      `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${userData.position.lotitude}&longitude=${userData.position.longitude}&localityLanguage=en`
-   )
-      .then((response) => response.json())
-      .then((data) => {
-         console.log(data);
-      });
-});
+// country.addEventListener("click", function () {
+//    fetch("https://restcountries.com/v3.1/name/deutschland")
+//       .then((response) => response.json())
+//       .then((data) => {
+//          console.log(data);
+//       });
+// });
+
+// infocountry.addEventListener("click", function () {
+//    fetch(
+//       `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${userData.position.lotitude}&longitude=${userData.position.longitude}&localityLanguage=en`
+//    )
+//       .then((response) => response.json())
+//       .then((data) => {
+//          console.log(data);
+//       });
+// });
