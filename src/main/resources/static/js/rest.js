8f9b4826dc1a14fee5df2cc65a7c79c3472e9399
bohdan <bohdenshoshka@gmail.com>
2023-12-01 10:57:24 +0200

    Паста карбонара з вершками, беконом і грибами • 1 столова ложка рослинної олії • 3 великі білі цибулі, нарізані кільцями • 600 г свіжих нарізаних грибів • 1 столова ложка олії • 800 г бекону • 8 яєць • 1 чашка жирних вершків • 1 чашка тертого сиру Пармезан • 800 г спагеті • Трохи петрушки • Мелений червоний перець Соус. Підсмажте цибулю на рослинній олії так, щоб вона стала золотистого відтінку, і викладіть зарум'янену цибулю на окрему тарілку або паперовий рушник. На тій же сковороді підсмажте гриби. Коли гриби будуть готові, додайте на сковороду бекон і цибулю. Продовжуйте підсмажувати на середньому вогні, поки бекон не набуде коричневого відтінку. В окремій тарілці збийте яйця, вершки і тертий сир. Отриману суміш вилийте на сковороду і готуйте, поки соус не загусне. Спагеті. Тим часом, відваріть спагеті і злийте воду. У готові спагеті додайте соус, петрушку і червоний перець. Страва готова!

--- src/main/resources/static/js/rest.js
+++ src/main/resources/static/js/rest.js
@@ -1,10 +1,40 @@
+// Vissual+
 const modalWrite = document.querySelector(".modalWrite");
-
 document.querySelector(".dbGUI .write").addEventListener("click", function(){
  modalWrite.classList.remove("--hidden");
 })
-
-
 document.querySelector(".modalWrite .overlay").addEventListener("click", function(){
    modalWrite.classList.add("--hidden");
 })
+// Visual-
+
+
+
+// Write
+const dbWrite = document.querySelector(".modalWrite input[type='button']");
+
+const inputs = {
+   name: document.querySelector("input[name='name']"),
+   age: document.querySelector("input[name='age']"),
+   job: document.querySelector("input[name='job']"),
+   get(){
+      return [this.name.value, this.age.value, this.job.value]
+   }
+}
+
+dbWrite.addEventListener("click",function(){
+   console.log(inputs);
+   console.log(inputs.get());
+   const data = inputs.get();
+   fetch("obamakiller/database/insert", {
+      method: "post",
+      body:{
+         name: data[0],
+         age: data[1],
+         job: data[2]
+      }
+   })
+})
+
+
+// Get
