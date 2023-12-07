ca9eca93340827bfa49dff14ed149715783c3747
bohdan <bohdenshoshka@gmail.com>
2023-12-07 16:40:04 +0200

    Бля буду этот чел с 2 чсти бензопилы злой как его так звали йошида чи шо? крч пиздец он тип подозрительный отвечаю что-то скрывает, хз может он и не злой но тайнит что-то по страшному

---
+++ src/main/resources/static/js/sockets.js
@@ -0,0 +1,52 @@
+var stompClient = null;
+
+function connect() {
+    var socket = new SockJS('/ws');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function(frame) {
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/public', function(response) {
+            showMessage(JSON.parse(response.body));
+        });
+    });
+    // axios.get("/messages").then(response => {
+    //   console.log(response);
+    //     for (const i of response.data) {
+    //         showMessage(i);
+    //         console.log(i);
+    //     }
+    // })
+}
+
+function sendMessage() {
+    console.log("oleg");
+    var messageInput = document.getElementById('messageInput');
+    var message = messageInput.value;
+    var sender = document.querySelector(".UI input[type='text']").value;
+    const color = document.querySelector(".UI input[type='color']").value;
+    console.log(color); 
+    stompClient.send("/app/send_message", {}, JSON.stringify({ 'content': message, 'sender': sender, 'color': color }));
+    // console.log(message, sender);
+    messageInput.value = '';
+}
+
+function showMessage(message) {
+    console.log(message);
+    var messages = document.querySelector('.messages');
+    var li = document.createElement('li');
+    
+    const author = document.createElement("span");
+    author.textContent = message.sender + ": ";
+    author.style.color = message.color || "red";
+
+    const messg = document.createElement("span");
+    messg.textContent = message.content;
+
+    li.appendChild(author);
+    li.appendChild(messg);
+    messages.appendChild(li);
+}
+
+showMessage({content: "oleg", sender:"olegich", color:"red"});
+
+connect();
